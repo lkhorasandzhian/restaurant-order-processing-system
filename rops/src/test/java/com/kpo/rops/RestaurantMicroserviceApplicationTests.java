6290bd8ce1244bac6408c13627b2f7d560dadcbd
lkhorasandzhian <levon.kh228@gmail.com>
2023-05-23 22:47:01 +0300

    initial Spring project was uploaded

---
+++ rops/src/test/java/com/kpo/rops/RestaurantMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpo.rops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
